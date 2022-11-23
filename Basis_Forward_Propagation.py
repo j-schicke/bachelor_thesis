12b5d0c4f8dc50a58b5ad7e1c839578b851a25ea
j-schicke <67542705+j-schicke@users.noreply.github.com>
2022-11-23 18:50:48 +0100

    Update Basis_Forward_Propagation.py

--- Basis_Forward_Propagation.py
+++ Basis_Forward_Propagation.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def newton_euler(w:array, ct, cq, d):
     m = np.array([ct, ct, ct, ct], [0, d*ct, 0, -d*ct], [-d*ct, 0, d*ct, 0], [cq, cq, cq, cq])
-    u = m * w
+    u = m * w**2
     return u
 
 
