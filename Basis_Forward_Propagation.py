aa8edb37f2c95c4b2d45854278f4afb05b013c26
j-schicke <Jana.schicke@campus.tu-berlin.de>
2022-12-12 17:08:19 +0100

    new

--- Basis_Forward_Propagation.py
+++ Basis_Forward_Propagation.py
@@ -23,13 +23,11 @@ def acceleration(m, u, z_w, z_b):
 
 
 def velocity(acc, vel, time, prev_time):
-    v = 0
     dt = time - prev_time
     v = vel + acc*dt
     return v
 
 def position(vel, pos, time, prev_time):
-    y = 0
     dt = time - prev_time
     y = pos + vel*dt
     return y
@@ -39,16 +37,14 @@ def angular_acc(u, wbw, I):
     return wbw_2
 
 def angular_velocity(acc_a, vel_a, time, prev_time):
-    v = 0
     dt = time - prev_time
     v = vel_a + acc_a*dt
     return v
 
-def angular_position(vel_a, pos_a, time, prev_time):
-    y = 0
+def new_quaternion(v, q, time, prev_time):
     dt = time - prev_time
-    y = pos_a + vel_a*dt
-    return y
+    quaternion = rowan.calculus.integrate(q, v, dt)
+    return quaternion
 
 if __name__ == '__main__':
     #parser = argparse.ArgumentParser()
@@ -66,23 +62,21 @@ if __name__ == '__main__':
     m = MultirotorConfig.MASS
     acc = []
     z_w = np.array([0,0,1])
-    R = rowan.to_matrix([data['stateEstimate.qx'][0], data['stateEstimate.qy'][0], data['stateEstimate.qz'][0], data['stateEstimate.qw'][0]])
     prev_time = 0
-    pos_a = []
     vel_a = []
     acc_a = []
     pos = []
     vel = []
     acc = []
-
+    quaternions = []
 
     vel_a.append(np.array([data['gyro.x'][0], data['gyro.y'][0], data['gyro.z'][0]]))
-    pos_a.append(np.array([0,0,0]))
     vel.append(np.array([data['stateEstimate.vx'][0], data['stateEstimate.vy'][0], data['stateEstimate.vz'][0]]))
     pos.append(np.array([data['stateEstimate.x'][0], data['stateEstimate.y'][0], data['stateEstimate.z'][0]]))
-
+    quaternions.append(np.array([data['stateEstimate.qw'][0],data['stateEstimate.qx'][0], data['stateEstimate.qy'][0], data['stateEstimate.qz'][0]]))
 
     for i in range(len(data['timestamp'])):
+        R = rowan.to_matrix(quaternions[i])
         z_b= np.asarray(R@z_w) 
         w = np.array([data['rpm.m1'][i], data['rpm.m2'][i], data['rpm.m3'][i], data['rpm.m4'][i]]) 
         u = newton_euler(w, ct, cq, d)
@@ -103,26 +97,33 @@ if __name__ == '__main__':
         v_a = angular_velocity(acc_a[i], vel_a[i] ,time, prev_time)
         vel_a.append(v_a)
 
-        y_a = angular_position(vel_a[i], pos_a[i], time, prev_time)
-        pos_a.append(y_a)
+        quat = new_quaternion(vel_a[i], quaternions[i], time, prev_time)
+        quaternions.append(quat)
 
         prev_time = time
+    quaternions.pop(len(data['timestamp']))
+    quaternions = np.array(quaternions)
     
-        R = rowan.to_matrix([data['stateEstimate.qx'][i], data['stateEstimate.qy'][i], data['stateEstimate.qz'][i], data['stateEstimate.qw'][i]])
 
-    pos = np.asarray(pos)
+    fig, ax = plt.subplots(2)
+    ax[0].plot(data['timestamp'], data['stateEstimate.qw'], '-', label='w')
+    ax[0].plot(data['timestamp'], data['stateEstimate.qx'], '-', label='x')
+    ax[0].plot(data['timestamp'], data['stateEstimate.qy'], '-', label='y')
+    ax[0].plot(data['timestamp'], data['stateEstimate.qz'], '-', label='z')
 
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    zline = pos[:,0]
-    xline = pos[:,1]
-    yline = pos[:,2]
-    ax.plot3D(xline, yline, zline, 'gray')
-    t = (data['timestamp'] - start_time) / 1000
+    ax[0].set_xlabel('timestamp [ms]')
+    ax[0].set_ylabel('quaternions')
+    ax[0].legend(loc=9, ncol=3, borderaxespad=0.)
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax[1].plot(data['timestamp'], quaternions[:,0], '-', label='w')
+    ax[1].plot(data['timestamp'], quaternions[:,1], '-', label='x')
+#   ax[1].plot(data['timestamp'], quaternions[:,2], '-', label='y')
+#   ax[1].plot(data['timestamp'], quaternions[:,3], '-', label='z')
 
+    ax[1].set_xlabel('timestamp [ms]')
+    ax[1].set_ylabel('new_quaternions')
+#   ax[1].legend(loc=9, ncol=3, borderaxespad=0.)
     plt.show()
-        
+
+    print(quaternions[2])
+    print(np.array([data['stateEstimate.qw'][2],data['stateEstimate.qx'][2], data['stateEstimate.qy'][2], data['stateEstimate.qz'][2]]))
